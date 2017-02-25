789d7da02f2eaf9ebcb3d732bbf7f4de7e58d91b
dina aziz <dina.aziz100@yahoo.com>
2017-02-25 18:50:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function theTruth()
+{return true;}
+
+function sayHey()
+{return "Hey!";}
+
+function sayHeyFriend(name)
+//   return "I love <NAME_OF_ICECREAM>";}
+{return `Hey, ${name}!`;}
+
+function favoriteIceCream(nameOfIcecream)
+{ return `I love ${nameOfIcecream}`;}
+
+function shouting(string)
+{return string.toUpperCase();}
+
+function roundDown(number)
+{ return Math.floor(number);
+ }
